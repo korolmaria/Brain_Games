23ec077bc1588466c75a33776817bbd464ac01eb
maria <mkweblife@mail.ru>
2020-11-12 09:05:23 +0300

    Add new functional 'greeting user'

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import promptly from 'promptly';
+
+const askNameUser = async () => {
+    const nameUser = await promptly.prompt('May I have your name? ');
+    console.log(`Hello, ${nameUser}!`);    
+}
+
+export default askNameUser;
