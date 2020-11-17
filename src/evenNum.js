bd01ca3923d5fa8c763f3b66997911a74500f63e
maria <mkweblife@mail.ru>
2020-11-17 11:45:21 +0300

    Add functional 'Congratulates'

--- src/evenNum.js
+++ src/evenNum.js
@@ -10,16 +10,21 @@ const isEvenNumber = (num) => {
 const askEvenNumber = () => {
   const nameUser = readlineSync.question('May I have your name? ');
   console.log(`Hello! ${nameUser}\nAnswer "yes" if the number is even, otherwise answer "no".`);
+  let countRightAnswer = 0;
   for (let i = 0; i < 3; i += 1) {
     const randomNum = randomInteger();
     console.log(`Question: ${randomNum}`);
     const answerUser = readlineSync.question('Your answer: ');
     if (answerUser === isEvenNumber(randomNum)) {
       console.log('Correct!');
+      countRightAnswer += 1;
     } else {
       console.log(`'${answerUser}' is wrong answer ;(. Correct answer was '${isEvenNumber(randomNum)}'.
             Let's try again, ${nameUser}!`);
     }
+  }
+  if (countRightAnswer === 3) {
+    console.log(`Congratulations, ${nameUser}!`);
   }
 };
 export default askEvenNumber;
