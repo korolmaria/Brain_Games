260f0da0e7df6c4e7ae767ed650b8a97ae988854
maria <mkweblife@mail.ru>
2020-11-23 14:20:14 +0300

    Changing realization - adding getArr() function

--- src/index.js
+++ src/index.js
@@ -1,91 +1,64 @@
 import readlineSync from 'readline-sync';
-import { questionEven, isEvenNumber } from '../games/evenNum.js';
-import { questionPrime, isPrimeNum } from '../games/primeNum.js';
-import { questionCalc, getCalcString, resultCalculate } from '../games/calcNum.js';
-import { getGcdNum, getGcdString, questionGcd } from '../games/gcdNum.js';
-import {
-  randomInteger, MIN, MAX, COUNTROUNDS,
-} from './cli.js';
-import {
-  questionProgression, getStringProgression, getProgressionAnswer, getArrayProgressive,
-} from '../games/progressionNum.js';
 
-const getQuestion = (nameGame) => {
-  let question = '';
-  switch (nameGame) {
-    case 'brainEven':
-      question += questionEven;
-      break;
-    case 'brainCalc':
-      question += questionCalc;
-      break;
-    case 'brainGcd':
-      question += questionGcd;
-      break;
-    case 'brainProgression':
-      question += questionProgression;
-      break;
-    case 'brainPrime':
-      question += questionPrime;
-      break;
-    default:
-      break;
-  }
-  return question;
+const COUNTROUNDS = 3;
+const MIN = 0;
+const MAX = 30;
+
+export const randomInteger = (minNum = 0, maxNum = 50) => {
+  const rand = minNum + Math.random() * (maxNum + 1 - minNum);
+  return Math.floor(rand);
 };
 
-const getResult = (itemName) => {
+const getArray = (countNums) => {
+  const arr = [];
+  if (countNums === 1) {
+    arr.push(randomInteger());
+  }
+  if (countNums === 2) {
+    arr.push(randomInteger());
+    arr.push(randomInteger());
+  }
+  if (countNums >= 5) {
+    const minLength = 5;
+    const maxLength = 10;
+    const lengthArray = randomInteger(minLength, maxLength);
+    const progresNumSum = randomInteger(MIN, MAX);
+    const numStart = randomInteger(MIN, MAX);
+    for (let i = 0; i < lengthArray; i += 1) {
+      arr[i] = (i === 0) ? numStart : arr[i - 1] + progresNumSum;
+    }
+  }
+  return arr;
+};
+
+const getRandIndex = (arr) => {
+  const randInd = randomInteger(0, arr.length);
+  return randInd;
+};
+
+export const getResult = (question, countNums, mathString, rightResult) => {
   console.log('Welcome to the Brain Games!');
   const nameUser = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${nameUser}`);
-  console.log(getQuestion(itemName));
+  console.log(question);
   let countRightAnswer = 0;
   for (let i = 0; i < COUNTROUNDS; i += 1) {
-    let rightAnswer = '';
-    let mathExpression = '';
-    const randNum1 = randomInteger(MIN, MAX);
-    const randNum2 = randomInteger(MIN, MAX);
-    const randomIndex = randomInteger(MIN, COUNTROUNDS - 1);
-    const arrProgression = getArrayProgressive();
-    const randIndexProgression = randomInteger(MIN, arrProgression.length - 1);
-    switch (itemName) {
-      case 'brainEven':
-        rightAnswer += isEvenNumber(randNum1);
-        mathExpression += randNum1;
-        break;
-      case 'brainCalc':
-        rightAnswer += resultCalculate(randNum1, randNum2, randomIndex);
-        mathExpression += getCalcString(randNum1, randNum2, randomIndex);
-        break;
-      case 'brainGcd':
-        rightAnswer += getGcdNum(randNum1, randNum2);
-        mathExpression += getGcdString(randNum1, randNum2);
-        break;
-      case 'brainProgression':
-        rightAnswer += getProgressionAnswer(arrProgression, randIndexProgression);
-        mathExpression += getStringProgression(arrProgression, randIndexProgression);
-        break;
-      case 'brainPrime':
-        rightAnswer += isPrimeNum(randNum1);
-        mathExpression += randNum1;
-        break;
-      default:
-        break;
-    }
+    const arrNums = getArray(countNums);
+    const randIndex = getRandIndex(arrNums);
+    const rightAnswer = rightResult(arrNums, randIndex);
+    const mathExpression = mathString(arrNums, randIndex);
     console.log(`Question: ${mathExpression}`);
-    const answer = readlineSync.question('Your answer: ');
-    if (answer === rightAnswer) {
+    const answerUser = readlineSync.question('Your answer: ');
+    if (answerUser === rightAnswer) {
       console.log('Correct!');
       countRightAnswer += 1;
     } else {
-      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${rightAnswer}".`);
+      console.log(`"${answerUser}" is wrong answer ;(. Correct answer was "${rightAnswer}".`);
       console.log(`Let's try again, ${nameUser}!`);
       break;
     }
-  }
-  if (countRightAnswer === COUNTROUNDS) {
-    console.log(`Congratulations, ${nameUser}!`);
+    if (countRightAnswer === COUNTROUNDS) {
+      console.log(`Congratulations, ${nameUser}!`);
+    }
   }
 };
-
-export default getResult;
