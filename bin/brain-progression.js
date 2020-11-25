06671f6e8fa488aabb55dee7e1fbea75da7760ee
maria <mkweblife@mail.ru>
2020-11-25 11:46:56 +0300

    Rewriting logis bin files for games

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,7 +1,2 @@
 #!/usr/bin/env node
-import { getResult } from '../src/index.js';
-import {
-  question, countNums, getMathString, getRightAnswer,
-} from '../games/progressionNum.js';
-
-getResult(question, countNums, getMathString, getRightAnswer);
+import '../games/progressionNum.js';
