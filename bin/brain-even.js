a25d54e1074c2ddbba486b95da639866ba5c2602
maria <mkweblife@mail.ru>
2020-11-25 11:05:11 +0300

    Rewriting logics of game brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,7 +1,2 @@
 #!/usr/bin/env node
-import { getResult } from '../src/index.js';
-import {
-  question, countNums, getMathString, getRightAnswer,
-} from '../games/evenNum.js';
-
-getResult(question, countNums, getMathString, getRightAnswer);
+import '../games/evenNum.js';
