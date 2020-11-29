96a2adda1a5cdae4227ca183329a2ffbe65258aa
Maria Korol <45860744+korolmaria@users.noreply.github.com>
2020-11-29 12:29:19 +0300

    Update primeNum.js

--- src/games/primeNum.js
+++ src/games/primeNum.js
@@ -4,21 +4,17 @@ import generateRandom from '../funcs.js';
 const questionOfGame = 'Answer "yes" if given number is prime. Otherwise answer "no"';
 
 const isPrime = (number) => {
-  const numsForAnalis = [];
-  let countDivisors = 0;
-  for (let i = 1; i <= number; i += 1) {
-    numsForAnalis.push(i);
+  if (number < 2) {
+    return false;
   }
-  for (let i = 0; i < numsForAnalis.length; i += 1) {
-    const currentNum = numsForAnalis[i];
-    if (number % currentNum === 0) {
-      countDivisors += 1;
+
+  for (let i = 2; i <= Math.sqrt(number); i += 1) {
+    if (number % i === 0) {
+      return false;
     }
   }
-  if (countDivisors === 2) {
-    return true;
-  }
-  return false;
+
+  return true;
 };
 
 const generateResult = () => {
